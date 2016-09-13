fc9cff2a58edb3d9e42c6dad5b5a34fb62c20591
georgewoizesko <woizesko@gmail.com>
2016-09-13 11:35:59 -0400

    First challenge one commit

--- challenge_1/challenge1.js
+++ challenge_1/challenge1.js
@@ -10,18 +10,34 @@
 var saveButton = $('#save-button');
 var loadButton = $('#load-button');
 
+
+// Save data to the current local store
+//localStorage.setItem('#save-me');
+
+// Access some stored data
+//alert( "#save-me = " + localStorage.getItem("#save-me"));
+
 /**
  * Gets the text from the element for you
  * @return {String}
  */
 function getText() {
-  return $('#save-me').val();
+	return $('#save-me').val();
 }
+
 
 /**
  * Puts different text
  * @param {String} text the stuff you want to put in the box
  */
 function setText(text) {
-  return $('#save-me').val(text);
+
+
+ return $('#save-me').val(text);
 }
+$('#save-button').click(function() {
+   localStorage.setItem('savedText', getText());
+})
+$('#load-button').click(function() {
+   setText(localStorage.getItem('savedText'));
+})
